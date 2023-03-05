8634b7c8ffcad8644982827122d8d18ab5fb255c
JaJablonski <kubas8826@gmal.com>
2023-03-05 13:39:14 +0100

    finished styling

--- pierwsze zadanie/study-buddy/src/views/Root.js
+++ pierwsze zadanie/study-buddy/src/views/Root.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import UsersList from 'components/UsersList/UsersList';
+import UsersList from 'components/organisms/UsersList/UsersList';
+import { ThemeProvider } from 'styled-components';
+import { GlobalSyle } from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
+import { Wrapper } from './Root.styles';
 
 const Root = () => (
-  <>
-    <UsersList />
-  </>
+  <ThemeProvider theme={theme}>
+    <GlobalSyle />
+    <Wrapper>
+      <UsersList />
+    </Wrapper>
+  </ThemeProvider>
 );
 
 export default Root;
